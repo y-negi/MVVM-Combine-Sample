18d058bc1e54a889ffdbc1a0cbe23b5bd832b8c0
根岸 裕太 <>
2020-08-29 09:36:56 +0900

    first commit

---
+++ MVVM-Combine-Sample/MVVM-Combine-SampleTests/MVVM_Combine_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_Combine_SampleTests.swift
+//  MVVM-Combine-SampleTests
+//
+//  Created by 根岸 裕太 on 2020/08/29.
+//  Copyright © 2020 根岸 裕太. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_Combine_Sample
+
+class MVVM_Combine_SampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
